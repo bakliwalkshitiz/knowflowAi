e8d2db3f065aa799c1f58a3114716426cec67ad6
bakliwalkshitiz <kshitizbakliwal72@gmail.com>
2026-08-01 13:47:22 +0530

    security(backend): externalize JWT secret, DB credentials, and CORS allowed origins

--- knowflow-ai/src/main/java/com/knowflow/KnowflowAiApplication.java
+++ knowflow-ai/src/main/java/com/knowflow/KnowflowAiApplication.java
@@ -1,8 +1,11 @@
 package com.knowflow;
 
+import com.knowflow.config.RagProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RagProperties.class)
 @SpringBootApplication
 public class KnowflowAiApplication {
 
